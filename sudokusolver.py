fb0f6f9dc595fb6ebb2a432213bcdc3cd299f65f
Francisco <fcojara506@gmail.com>
2021-06-05 22:11:48 -0400

    new algorithm
    Random numbers are selected from a sample of the permutation of required numbers

--- sudokusolver.py
+++ sudokusolver.py
@@ -1,16 +1,20 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import copy
-import os
 import imageio
 import numpy.random as rnd
-from smt.sampling_methods import LHS
+import random
+import glob
+from celluloid import Camera 
+from IPython.display import HTML
 
-
+import os
+    
 unsolved = np.array(
     [3, 9, 6, 0, 8, 0, 7, 1, 5, 0, 0, 7, 9, 6, 1, 0, 0, 3, 8, 4, 1, 0, 3, 0, 2, 0, 0, 6, 3, 0, 1, 0, 0, 5, 2, 0, 1, 0,
      0, 7, 2, 5, 3, 9, 0, 0, 5, 2, 6, 9, 3, 8, 0, 1, 4, 0, 8, 3, 7, 6, 0, 0, 2, 2, 0, 5, 8, 0, 9, 0, 3, 4, 9, 6, 3, 0,
      0, 2, 0, 7, 8])
+
 
 def cost_function(x,unsolved=unsolved):
     # function to determine the duplicate values in the Sudoku
@@ -32,29 +36,33 @@ def cost_function(x,unsolved=unsolved):
     unsolved_copy = np.reshape(unsolved_copy, (dim_sudoku, dim_sudoku))
     
     # Determinate duplicates
-    num_duplicates = 0  # initilise num_duplicates                                
+                                 
     # duplicates in rows
+    num_duplicates_row = 0  # initilise num_duplicates   
     for irow in range(dim_sudoku):
         row = unsolved_copy[irow, :] # select each row
         unique_values = set(row) # find the unique values and sort them
-        num_duplicates += dim_sudoku - len(unique_values)  #
-
+        num_duplicates_row += dim_sudoku - len(unique_values)  #
+    
     # duplicates in columns
+    num_duplicates_col = 0
     for icol in range(dim_sudoku):
         col = unsolved_copy[:, icol] # select each column
         unique_values = set(col) # find the unique values and sort them
-        num_duplicates += dim_sudoku - len(unique_values)  # 
+        num_duplicates_col += dim_sudoku - len(unique_values)  # 
 
     # duplicates in each 3x3 grid quadrant
+    num_duplicates_quad=0
     for i in range(3):
         for j in range(3):
             kernal = unsolved_copy[i * 3:(i * 3) + 3,
                                    j * 3:(j * 3) + 3] # select each subwindow
             kernal = np.reshape(kernal, dim_sudoku) # transform to vector
             unique_values = set(kernal) # find the unique values and sort them
-            num_duplicates += dim_sudoku - len(unique_values)
+            num_duplicates_quad += dim_sudoku - len(unique_values)
+    num_duplicate=num_duplicates_row+num_duplicates_col+num_duplicates_quad
             
-    return num_duplicates
+    return num_duplicate,num_duplicates_row,num_duplicates_col,num_duplicates_quad
 
 
 
@@ -86,11 +94,12 @@ def plot_util(unsolved, sample_best_values, fstore, iteration):
     a = np.reshape(a, (9, 9))
     fig, axs = plt.subplots(1, 2, figsize=(8, 4))
     plt.subplots_adjust(left=0.05, right=0.95)
-    axs[0].set_title('Current Sudoku')
+    axs[0].set_title('Current best Sudoku')
     axs[0].axis('off')
     axs[1].set_xlabel('Iteration')
     axs[1].grid('True')
     axs[1].set_ylabel('Errors')
+    #axs[1].set_yscale('log')
     for i in range(10):
         if i == 0 or i == 3 or i == 6 or i == 9:
             axs[0].plot([0, 9], [i, i], color='k', linewidth=3)
@@ -109,21 +118,52 @@ def plot_util(unsolved, sample_best_values, fstore, iteration):
     fig.savefig('img/'+str(iteration) + '.png')
     return
 
-def sample_error_col(sample_size,sample_matrix,f=cost_function):
+def sample_error_col(sample_size,sample_matrix):
     ''' p: population size '''
     
     # compute the duplicate for each set of random samples
-    sample_num_duplicates = np.zeros((sample_size, 1))
-    for i in range(sample_size): sample_num_duplicates[i, :] = f(sample_matrix[i, :])
+    sample_num_duplicates = np.zeros((sample_size, 4))
+    for i in range(sample_size): sample_num_duplicates[i, :] = cost_function(sample_matrix[i, :])
     # concatenate random sample values and duplicate number
     sample_matrix = np.concatenate((sample_matrix, sample_num_duplicates), axis=1)
     return sample_matrix
 
 def sample_min_error(sample_error_col_obj):
-    return sample_error_col_obj[np.argmin(sample_error_col_obj[:, -1])] 
-    
+    return sample_error_col_obj[np.argmin(sample_error_col_obj[:, -1])]
 
-def evolution(f=cost_function, p = 50, it = 10, cull_percen = 0.5, mut_percen = 0.01, unsolved=unsolved,initial_condition=[0]):
+
+def random_permutation(iterable,sample_number=10):
+    "Random selection from itertools.permutations(iterable, r)"
+    pool = tuple(iterable)
+    r = len(pool)
+    return np.array([random.sample(pool, r) for i in range(sample_number)])
+
+def save_images2gifv1():
+    directory_files=glob.glob('img/*png')
+    images = []
+    for filename in directory_files:
+        images.append(imageio.imread(filename))
+        #os.remove(filename)
+        imageio.mimsave('SudokuSolve.gif', images)
+        
+def delete_img():
+        directory_files=glob.glob('img/*png')
+        for f in directory_files:
+            os.remove(f)
+       
+def save_images2gif():
+    directory_files=glob.glob('img/*png')
+    fig, ax = plt.subplots() # make it bigger
+    camera = Camera(fig)# the camera gets our figure            
+    for filename in directory_files:
+        img_obj = plt.imread(filename)
+        ax.imshow(img_obj) # plotting
+        camera.snap()
+    animation = camera.animate()
+    save('filename.gif') 
+
+        
+def evolution2(p = 20, it = 100000, cull_percen = 0.99, mut_percen = 0.05, unsolved=unsolved,initial_condition=[0]):
     '''
     INPUTS:
     f               : sudoku cost function
@@ -132,15 +172,14 @@ def evolution(f=cost_function, p = 50, it = 10, cull_percen = 0.5, mut_percen = 
     cull_percen     : percentage of population to be culled
     mut_percen      : percentage chance of a mutation
     unsolved        : the unsolved sudoku vector
+    initial_condition: initial guess of the unsolved numbers
     
     OUTPUTS:
     sample_best_values          : the vector of optimised input values
-    SudokuSolve.gif : a gif showing function value over time as
-                        well as the current sudoku
 
     Default values
-    f=cost_function
-    p=1000
+
+    p=20
     it=1000
     cull_percen= 0.8
     mut_percen=0.05
@@ -148,12 +187,15 @@ def evolution(f=cost_function, p = 50, it = 10, cull_percen = 0.5, mut_percen = 
 
     '''
     print("Solving Sudoku")
-    empty_entries = unsolved.size - np.count_nonzero(unsolved) # count zeros
-    bounds = np.tile([1, 9],(empty_entries,1))
-
-    '''ORIGINAL SAMPLE of quasi-random Latin Hypercubre Sampling (LHS)'''
-    sampling = LHS(xlimits=bounds)  # LHS
-    sample_random_values = sampling(p).astype('int') #LHS sample given the population size
+    delete_img() # delete previous saved images
+   
+    unique_items, unique_counts = np.unique(unsolved, return_counts=True) # count unique 0,1,2,3,4,5,6,7,8,9
+    empty_entries = unique_counts[0] # count zeros
+    unique_items=unique_items[1:]
+    unique_counts=9-unique_counts[1:]
+    
+    initial_sample = np.repeat(unique_items,unique_counts,axis=0)
+    sample_random_values=random_permutation(initial_sample,p)
     
     '''EVALUATING FITNESSES'''
     # compute error (duplicated values) and add the last column
@@ -169,21 +211,22 @@ def evolution(f=cost_function, p = 50, it = 10, cull_percen = 0.5, mut_percen = 
     val_store= []    
     for iteration in range(it):
         '''TOURNAMENT SELECTION: subset of random values'''
-        sample_random_values_subset = np.zeros((int(p * (cull_percen)), empty_entries + 1))  # PARAMETER HERE (percentage to be kept)
-
+        sample_random_values_subset = np.zeros((int(p * (cull_percen)), empty_entries + 4))  # PARAMETER HERE (percentage to be kept)
+        
+        
         for new_count in range(len(sample_random_values_subset)):
             rnd.shuffle(sample_random_values)
             t_size = rnd.randint(1, 10)  # SORT OF PARAMETER HERE (tournament size)
             t = sample_random_values[:t_size, :] # select a subset of the random sample
-            t_best = t[np.argmin(t[:, -1])] # determine the lowest duplicate set
+            t_best = t[np.argmin(t[:, -4])] # determine the lowest duplicate set
             sample_random_values_subset[new_count, ] = t_best # fill subset with the best
 
         sample_random_values = copy.deepcopy(sample_random_values_subset)# selected subset
         
         '''COMPLEMENT WITH RANDOM CANDIDATES'''
         new_psize = p - len(sample_random_values)
-        sampling = LHS(xlimits=bounds) # LHS
-        sample_random_values_subset = sampling(new_psize).astype('int')  #LHS sample given the new size
+        sample_random_values_subset = random_permutation(initial_sample,new_psize)  #sample given the new size
+        
         # compute the duplicate for each set of new random samples
         sample_random_values_subset=sample_error_col(new_psize, sample_random_values_subset)
         
@@ -193,46 +236,37 @@ def evolution(f=cost_function, p = 50, it = 10, cull_percen = 0.5, mut_percen = 
         sample_random_values = np.concatenate((sample_random_values_subset, sample_random_values), axis=0)
         
         # determine the best sample from selection+complement
-        best_index = np.argmin(sample_random_values[:, -1])
+        best_index = np.argmin(sample_random_values[:, -4])
         sample_best_values = sample_random_values[best_index]
-        sample_best_values_val = sample_random_values[best_index, -1]
-        
-        print('iteration:'+str(iteration)+ ' errors:'+str(sample_best_values_val), end='\n')
-        fstore.append(sample_best_values_val.astype('int'))
-        val_store.append(sample_best_values.astype('int'))
-        '''CROSSOVER HERE: change the order of the columns in each row'''
+        sample_best_values_val = sample_random_values[best_index, range(-4,0)]
         
         
-        
-        rnd.shuffle(sample_random_values)
+        #rnd.shuffle(sample_random_values)
         '''MUTATION CODE HERE'''
         for i in range(len(sample_random_values)):
             for j in range(empty_entries):
                 prob = rnd.uniform()
-                #print(np.argmin(sample_random_values[:, -1]))
                 if prob < mut_percen and best_index!=i :
-                    sample_random_values[i, j] = rnd.uniform(bounds[j, 0], bounds[j, 1])
+                    sample_random_values[i, j] = rnd.randint(1,10)
                     
         
-        
-        sample_random_values = sample_random_values[:, :-1] # delete the last column (duplicates number)
+        sample_random_values = sample_random_values[:, :-4] # delete the last column (duplicates number)
         sample_random_values = sample_error_col(p, sample_random_values) # calculate new errors
         
         
         # plot the current Sudoku and error per iteraton
-        if it<5: plot_util(unsolved, sample_best_values, fstore, iteration)
-    if it<5:   
-        images = []
-        for filename in range(it):
-            images.append(imageio.imread('img/'+ str(filename) + '.png'))
-            #os.remove('img/'+ str(filename) + '.png')
-        imageio.mimsave('SudokuSolve.gif', images)
-    
+        if iteration%100==0: print('iteration:'+str(iteration)+ ' errors:'+str(sample_best_values_val), end='\n')
+        fstore.append(sample_best_values_val.astype('int'))
+        val_store.append(sample_best_values.astype('int'))
+        if iteration%10==0: plot_util(unsolved, sample_best_values, fstore, iteration)
+        
+        if sample_best_values_val[0]==0: break
+
     return val_store
 
-solved= [np.array([2, 4, 5, 2, 4, 8, 5, 7, 6, 9, 9, 4, 8, 7, 8, 4, 6, 7, 4, 1, 9, 5, 7, 1, 6, 4, 5, 1, 0]),
-         np.array([2, 4, 5, 2, 4, 8, 5, 7, 6, 9, 8, 4, 8, 7, 8, 4, 6, 7, 4, 1, 9, 5, 7, 1, 6, 4, 5, 1, 9])]
 
-#solved = evolution(f=cost_function, p = 50, it = 100, cull_percen = 1, mut_percen = 0.01, unsolved=unsolved)
-for i in range(1000):
-    solved = evolution(f=cost_function, p = 100, it = 100, cull_percen = 1, mut_percen = 0.05, unsolved=unsolved, initial_condition=solved[-1])
+#solved= [np.array([2, 4, 5, 2, 4, 8, 5, 7, 6, 9, 9, 4, 8, 7, 8, 4, 6, 7, 4, 1, 9, 5, 7, 1, 6, 4, 5, 1, 0, 0,0,0]),
+#         np.array([2, 4, 5, 2, 4, 8, 5, 7, 6, 9, 8, 4, 8, 7, 8, 4, 6, 7, 4, 1, 9, 5, 7, 1, 6, 4, 5, 1, 9])]
+
+solved = evolution2(p = 20, it = 100000, cull_percen = 0.999, mut_percen = 0.05, unsolved=unsolved)
+save_images2gifv1()
